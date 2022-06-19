4646a0894548658ab08d2939d532d6e492b4693c
Dennis McCamily <dmccamily4@gmail.com>
2022-06-19 19:35:59 +0100

    Update latest.sh

--- latest.sh
+++ latest.sh
@@ -5,11 +5,8 @@
 /usr/bin/curl -s -w 'Testing Connection Response Time for: github.com\n\nLookup Time:\t\t%{time_namelookup}\nConnect Time:\t\t%{time_connect}\nPre-transfer Time:\t%{time_pretransfer}\nStart-transfer Time:\t%{time_starttransfer}\n\nTotal Time:\t\t%{time_total}\n' -o /dev/null -L https://raw.githubusercontent.com/MissingSyntax/Sphinx-Protection/master/latest.sh
 
 # Install DDoS Proxy Protection:
-if [ "$1" != "" ]; then
-latest_version="$1"
-else
-latest_version=`/usr/bin/curl -L https://raw.githubusercontent.com/MissingSyntax/Sphinx-Protection/master/CHANGELOG.txt|grep '*Sphinx DDoS-' |awk 'NR==1' |tr -d '*vDDoS-'|tr -d ':'`
-fi
+latest_version="2.3.3"
+
 /usr/bin/curl -L https://raw.githubusercontent.com/MissingSyntax/Sphinx-Protection/master/vddos-$latest_version.tar.gz -o vddos-$latest_version.tar.gz
 
 originhash=`curl -L https://raw.githubusercontent.com/MissingSyntax/Sphinx-Protection/master/md5sum.txt --silent | grep "vddos-$latest_version.tar.gz" |awk 'NR=1 {print $1}'`
